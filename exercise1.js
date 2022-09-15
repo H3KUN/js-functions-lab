f503c0d065edd0410b9e0b7d0492883a7508dbe6
Hiroto Iwata <hiroto1610@outlook.com>
2022-09-15 19:22:28 +0100

    Initial structure.

--- exercise1.js
+++ exercise1.js
@@ -1,7 +1,7 @@
 fetch("https://jsonplaceholder.typicode.com/todos")
     .then(response => response.json())
     .then(json => {
-        const completed = null // Complete this code
+        const completed = json // Complete this code
         completed.forEach( (todo, index) => {
             console.log(`${todo.title} - ${todo.completed}`)
         })
