96d06f78cbf3942eceb7e1995d4ebda5f6844ddb
Hiroto Iwata <hiroto1610@outlook.com>
2022-09-15 19:24:08 +0100

    Exercise 1.

--- exercise1.js
+++ exercise1.js
@@ -1,7 +1,7 @@
 fetch("https://jsonplaceholder.typicode.com/todos")
     .then(response => response.json())
     .then(json => {
-        const completed = json // Complete this code
+        const completed = json // Complete this code <=Completed :)
         completed.forEach( (todo, index) => {
             console.log(`${todo.title} - ${todo.completed}`)
         })
